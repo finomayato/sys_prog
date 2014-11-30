5c37ad2f18019ed4a7ed63de91b02e2b9452f876
char-lie <webmaster976@gmail.com>
2014-11-30 10:41:05 +0200

    Completed lab2

--- lab2/main.c
+++ lab2/main.c
@@ -1,60 +1,84 @@
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void *functionC();
+
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutexattr_t   mta;
+
+// Globals! Achtung! Keep your children away!
+int counter = 0;
+int result = 0;
+const int ITERATIONS_NUMBER = 1E6;
 
 void showPshared(pthread_mutexattr_t *mta) {
-  int           rc;
-  int           pshared;
+    int           rc;
+    int           pshared;
  
-  printf("Check pshared attribute\n");
-  rc = pthread_mutexattr_getpshared(mta, &pshared);
-  //checkResults("pthread_mutexattr_getpshared()\n", rc);
+    printf("Check pshared attribute\n");
+    rc = pthread_mutexattr_getpshared(mta, &pshared);
+    //checkResults("pthread_mutexattr_getpshared()\n", rc);
  
-  printf("The pshared attributed is: ");
-  switch (pshared) {
-  case PTHREAD_PROCESS_PRIVATE:
-    printf("PTHREAD_PROCESS_PRIVATE\n");
-    break;
-  case PTHREAD_PROCESS_SHARED:
-    printf("PTHREAD_PROCESS_SHARED\n");
-    break;
-  default : 
-    printf("! pshared Error !\n");
-    exit(1); 
-  }
-  return;
+    printf("The pshared attributed is: ");
+    switch (pshared) {
+        case PTHREAD_PROCESS_PRIVATE:
+            printf("PTHREAD_PROCESS_PRIVATE\n");
+            break;
+        case PTHREAD_PROCESS_SHARED:
+            printf("PTHREAD_PROCESS_SHARED\n");
+            break;
+          default : 
+            printf("! pshared Error !\n");
+            exit(1);
+    }
+    return;
 }
 
-int main(int argc, char **argv)
+main()
 {
-  int                   rc=0;
-  pthread_mutexattr_t   mta;
-  int                   pshared=0;
-  pthread_mutex_t       mut=PTHREAD_MUTEX_INITIALIZER;
-  pthread_mutex_init(&mut, &mta);
+    //printf("%ld\n", _POSIX_THREAD_PROCESS_SHARED);
+    pthread_mutex_init(&mutex1, &mta);
+    int ps = pthread_mutexattr_setpshared(&mta, PTHREAD_PROCESS_SHARED);
+    printf("%d\n", ps);
+    int rc1, rc2;
+    int i;
+    pthread_t thread1, thread2;
+    unsigned int threadsNumber;
+    printf("Enter the number of threads (please, don't be cruel to yourself, "
+            "enter the number between 1 and 10) ");
+    scanf("%u", &threadsNumber);
+    if (threadsNumber < 1 || threadsNumber > 1000) {
+        printf("You shouldn't have done it!\n");
+        return(EXIT_FAILURE);
+    }
+    // A little bit of pain
+    pthread_t* threads = (pthread_t*)malloc(sizeof(pthread_t)*threadsNumber);
+    i = -1;
+    while (++i < threadsNumber) {
+        if (pthread_create( &threads[i], NULL, &functionC, NULL)) {
+            printf("Thread %d creation failed :(", i);
+        }
+    }
 
-  printf("Entering testcase\n");
+    i = -1;
+    while (++i < threadsNumber) {
+        pthread_join( threads[i], NULL);
+    }
 
-  /*
-  printf("Create a default mutex attribute\n");
-  rc = pthread_mutexattr_init(&mta);
-  //checkResults("pthread_mutexattr_init()\n", rc);
-  showPshared(&mta);
-  */
+    showPshared(&mta);
+    // This is Sparta!
+    exit(EXIT_SUCCESS);
+}
 
-  printf("Change pshared attribute\n");
-  rc = pthread_mutexattr_setpshared(&mta, PTHREAD_PROCESS_SHARED);
-  //checkResults("pthread_mutexattr_setpshared()\n", rc);  
-  showPshared(&mta);
-  //pthread_mutex_lock(&mut);
- 
-  int a;
-  scanf("%d", &a);
-  //pthread_mutex_unlock(&mut);
-  printf("Destroy mutex attribute\n");
-  rc = pthread_mutexattr_destroy(&mta);
-  //checkResults("pthread_mutexattr_destroy()\n", rc);
- 
-  printf("Main completed\n");
-  return 0;
+
+void *functionC()
+{
+    int v;
+    pthread_mutex_lock(&mutex1);
+    printf("Enter some integer value ");
+    scanf("%d", &v);
+
+    pthread_mutex_unlock(&mutex1);
 }
